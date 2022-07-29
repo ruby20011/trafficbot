e611ed6059f23ed9b6dce853816f6a49d4052e35
ruby20011 <110215194+ruby20011@users.noreply.github.com>
2022-07-29 05:41:47 +0100

    Update and rename bot to bot.py

---
+++ bot.py
@@ -0,0 +1,20 @@
+
+import webbrowser
+import time
+x = 1
+while True:
+  webbrowser.open('https://ruby20011.github.io/')
+  time.sleep(105)
+  webbrowser.open_new_tab('https://ruby20011.github.io/')
+  time.sleep(30)
+  webbrowser.open_new_tab('https://ruby20011.github.io/')
+  time.sleep(30)	
+  webbrowser.open_new_tab('https://ruby20011.github.io/')
+  time.sleep(30)
+  webbrowser.open_new_tab('https://ruby20011.github.io/')
+  time.sleep(30)	
+  webbrowser.open_new_tab('https://ruby20011.github.io/')
+  time.sleep(30)
+  x +=1
+	
+	
